88cb612175f9bca938f0e3058cee7a2adb145a88
Tyler Silva <tylerbsilva@gmail.com>
2015-05-20 13:15:38 -0500

    Added ability to add items to the item list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-$(document).ready(function {
-  alert("TESTING");
+$(document).ready(function() {
+
+  $(".itemInput").focus();
+
+  $(document)
+
+  // Adding an item
+  .on('click', '.addItem', function(){
+    var item = $('input[name="itemInput"]').val().trim();
+    if (item) {
+      $(".itemList").append("<li><input type='checkbox'>" + item + "<button>X</button></li>");
+    }
+    $(".itemInput").val('').focus();
+
+  });
+
+  $('.addItem').submit( function(e) {
+        e.preventDefault();
+
+        return false;
+    });
 });
